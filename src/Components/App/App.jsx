c252cf27d4e98b3239eb1cb7f0d6134b008f318c
RBord <rostyslavbord@gmail.com>
2021-07-05 19:29:41 +0300

    Второй шаг - добавил номер телефона

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -4,26 +4,34 @@ import shortid from 'shortid';
 class App extends React.Component {
     state = {
         contacts: [],
-        name: ''
+        name: '',
+        number: '',
     }
 
     nameInputId = shortid.generate();
+    numberInputId = shortid.generate();
 
     addContact = () => {
         const contact = {
             id: shortid.generate(),
             name: this.state.name,
+            number: this.state.number,
         }
 
         this.setState(prevState => ({
             contacts: [contact, ...prevState.contacts]
         }))
     }
-
+    
+    // reset = () => {
+    //     this.setState({ name: '' });
+    // }
     handleNameChange = evt => {
         this.setState({ name: evt.currentTarget.value });
     }
-    
+    handleNumberChange = evt => {
+        this.setState({ number: evt.currentTarget.value });
+    }
     handleSubmit = evt => {
         evt.preventDefault();
         this.addContact();
@@ -46,13 +54,25 @@ class App extends React.Component {
                             required
                         />
                     </label>
-                    <button type='submit'>Add contact</button>
+                    <label htmlFor={this.numberInputId}>Number
+                        <input
+                            type="tel"
+                            name="number"
+                            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+                            onChange={this.handleNumberChange}
+                            id={this.numberInputId}
+                            required
+                            
+                        />
+                    </label>
+                    <button type='submit' >Add contact</button>
                 </form>
                 <h2>Contacts</h2>
                 <ul>
-                    {contacts.map(({id, name}) => {
+                    {contacts.map(({id, name, number}) => {
                         return (
-                            <li key={id}>{name}</li>
+                            <li key={id}>{name}: {number}</li>
                         )
                     })}
                 </ul>
